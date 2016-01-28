4350d3f21a7c73708b8abaf994df6c976d96efed
Jesse McGil Allen <jesse@jessemcgilallen.com>
2016-01-27 19:35:34 -0600

    initial commit for Hello World

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+/**
+ * Created by jessemcgilallen on 1/20/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+
